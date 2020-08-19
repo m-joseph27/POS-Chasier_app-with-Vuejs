5088cdf6f8f74d3cb8cea75b93348741facb8384
m-joseph27 <muh.joseph27@gmail.com>
2020-08-19 22:30:28 +0700

    add vuex

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,100 +1,46 @@
 <template>
 <div class="super-class">
   <div class="all-card">
-    <div @click="selectedItem(menu)" v-for="menu in menu" :key="menu.id_menu" class="parent-card">
+    <div v-for="menu in getMenu" :key="menu.id_menu"
+      @click="isSelected(menu)" class="parent-card">
       <div class="card-image">
-        <img :src="menu.menu_img">
+        <img :src="menu.menu_img" alt="menu">
       </div>
-      <div class="card-label">{{menu.menu_name}}</div>
-      <div class="card-price">{{menu.price}}</div>
+      <div class="card-label">
+        <p>{{ menu.menu_name }}</p>
+      </div>
+      <div class="card-price">
+        <p>{{ menu.price }}</p>
+      </div>
     </div>
-  </div>
-  <div class="page-controler">
-    <nav aria-label="Page navigation example">
-      <ul class="pagination justify-content-center">
-        <li class="page-item">
-        <a class="page-link" style="cursor:pointer" @click="prevPages">Previous</a>
-        </li>
-        <li class="page-item" v-for="pagination in totalPage" :key="pagination">
-          <a class="page-link" style="cursor:pointer"
-          @click="pages(pagination)"  >{{pagination}}</a></li>
-        <li class="page-item next">
-        <a class="page-link" style="cursor:pointer" @click="nextPages">Next</a>
-        </li>
-      </ul>
-    </nav>
   </div>
 </div>
 </template>
 
 <script>
-import axios from 'axios';
 
 export default {
   name: 'menu',
   data() {
     return {
-      menu: [],
-      currentPage: 1,
-      totalPage: [],
-      url: 'localhost:2000/api/v1/menu?page=',
+      selectedMenu: [],
     };
   },
+
   methods: {
-    selectedItem(item) {
-      this.$store.commit('selectedItem', item);
-    },
-    pages(id) {
-      this.currentPage = 0 + id;
-      axios.get(`http://localhost:2000/api/v1/menu?page=${this.currentPage}`)
-        .then((res) => {
-          // eslint-disable-next-line prefer-destructuring
-          this.menu = res.data.result[2];
-        });
-    },
-    nextPages() {
-      if (this.currentPage === this.totalPage) {
-        this.currentPage = this.totalPage;
-      } else {
-        this.currentPage += 1;
-      }
-      axios.get(`http://localhost:2000/api/v1/menu?page=${this.currentPage}`)
-        .then((res) => {
-          // eslint-disable-next-line prefer-destructuring
-          this.menu = res.data.result[2];
-          // eslint-disable-next-line prefer-destructuring
-          this.totalPage = res.data.result[0];
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-    prevPages() {
-      if (this.currentPage === 1) {
-        this.currentPage = 1;
-      } else {
-        this.currentPage -= 1;
-      }
-      axios.get(`http://localhost:2000/api/v1/menu?page=${this.currentPage}`)
-        .then((res) => {
-          // eslint-disable-next-line prefer-destructuring
-          this.menu = res.data.result[2];
-          // eslint-disable-next-line prefer-destructuring
-          this.totalPage = res.data.result[0];
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+    isSelected(item) {
+      this.$store.commit('selectedItem', { item, count: 1 });
     },
   },
+
   mounted() {
-    axios.get(`http://localhost:2000/api/v1/menu?page=${this.currentPage}`)
-      .then((res) => {
-        // eslint-disable-next-line prefer-destructuring
-        this.menu = res.data.result[2];
-        // eslint-disable-next-line prefer-destructuring
-        this.totalPage = res.data.result[0];
-      });
+    this.$store.dispatch('GETMENU');
+  },
+
+  computed: {
+    getMenu() {
+      return this.$store.state.menu;
+    },
   },
 
 };
@@ -105,10 +51,11 @@ export default {
 .super-class{
   background-color: rgba(190, 195, 202, 0.3);
   width: 100%;
-  height: 100vh;
+  height: 100%;
   z-index: 2;
   padding-left:180px;
   padding-top: 80px;
+  padding-bottom: 30px;
   .all-card{
     display: flex;
     flex-wrap: wrap;
@@ -132,6 +79,7 @@ export default {
       height: 80%;
       border-radius: 10px 10px 0 0;
       transition: ease-out .3s;
+      overflow: hidden;
       &:hover{
         transform: scale(1.1);
         transition: ease-out .3s;
@@ -145,15 +93,18 @@ export default {
     .card-label{
       width: 100%;
       height: 10%;
+      p{
+        color: #F24F8A;
+      }
     }
     .card-price{
       width: 100%;
       height: 10%;
       font-weight: bold;
+      p{
+        color: #57CAD5;
+      }
     }
-  }
-  .page-controler{
-    margin-top: 50px;
   }
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,39 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    menu: [],
     selectedItem: [],
   },
+
   mutations: {
-    selectedItem(state, item) {
-      state.selectedItem.push(item);
+    selectedItem(state, { item, count }) {
+      const items = state.selectedItem
+        .find((clicked) => clicked.item.id_menu);
+      if (!items) {
+        state.selectedItem.push({ item, count });
+      }
+    },
+    MENU(state, data) {
+      state.menu = data;
     },
   },
+
   actions: {
+    GETMENU(context) {
+      axios
+        .get(`${process.env.VUE_APP_URL}menu`)
+        .then((res) => {
+          context.commit('MENU', res.data.result);
+        });
+    },
   },
-  modules: {
+
+  getter: {
   },
+
 });
